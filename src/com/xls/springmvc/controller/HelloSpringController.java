970cb7d6ad461d54f4a96e1d1f4851d23587e169
xushanshan <978559694@qq.com>
2018-02-03 11:00:22 +0800

    ''

---
+++ src/com/xls/springmvc/controller/HelloSpringController.java
@@ -0,0 +1,15 @@
+package com.xls.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class HelloSpringController {
+	@RequestMapping("/hello")
+	public String test() {
+		System.out.println(" ..... hello");
+		return "hello";
+	}
+	
+}
